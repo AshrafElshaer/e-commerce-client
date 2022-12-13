c83534997d0ca08be33e44811dc7f5ae1b74e170
Ashraf Elshaer <ashrafelshaer98@icloud.com>
2022-12-12 20:33:32 -0600

    sort products by isNew first from rtk categories api

--- src/features/api/apiSlice.ts
+++ src/features/api/apiSlice.ts
@@ -1,6 +1,8 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import type { TCategory } from "../categories/categoriesSlice";
 
+export type TMeta = { requestId: string; timestamp: number };
+
 const serverUrl = import.meta.env.VITE_SERVER_URL;
 export const apiSlice = createApi({
   reducerPath: "api",
@@ -10,6 +12,12 @@ export const apiSlice = createApi({
       query: () => ({
         url: "/categories",
         responseHandler: (response) => response.json(),
+        transformResponse: (returnValue: TCategory[], meta: TMeta) => {
+          if (!meta) return [];
+          return returnValue.map((category) =>
+            category.products.sort((a, b) => Number(b.isNew) - Number(a.isNew))
+          );
+        },
       }),
     }),
 
